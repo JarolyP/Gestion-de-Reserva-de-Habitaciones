12709e0f11da8afe5a170836a2a0f946230b9ffd
JarolyP <jobs13az@gmail.com>
2024-03-19 18:44:47 -0400

    Haciendo la interfaces y haciendo la conexión de nuestro proyecto a BD

---
+++ SistemaReservaHotel/src/Logica/conexion.java
@@ -0,0 +1,31 @@
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conexion {
+    public String db = "new_schema1";
+    public String url = "jdbc:mysql://127.0.0.1/"+db;
+    public String user="root";
+    public String pass="";
+    
+    public conexion(){
+        
+    }
+    
+    public Connection conectar(){
+        Connection link = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            link = DriverManager.getConnection(this.url, this.user, this.pass);
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            
+        }
+        
+        return link;
+    }
+}
